3f73a50a1ddac0265076a580180a92d992ba72bd
Vinicius <vinniciussramallho98@gmail.com>
2025-06-11 15:29:06 -0300

    username, email and password default in bd

--- node/prisma/seed.js
+++ node/prisma/seed.js
@@ -1,100 +1,38 @@
-// const { PrismaClient } = require('@prisma/client');
-// const { faker } = require('@faker-js/faker');
-// const prisma = new PrismaClient();
+const { PrismaClient } = require('@prisma/client');
+const { faker } = require('@faker-js/faker');
+const bcrypt = require('bcryptjs');
 
-// async function main() {
-//   console.log(" Inserindo dados...");
+const prisma = new PrismaClient();
 
-//   // Criar zonas
-//   const zonas = [];
-//   for (let i = 0; i < 5; i++) {
-//     const zona = await prisma.zona.create({
-//       data: {
-//         nome_da_zona: `Zona ${i + 1}`,
-//         qtd_coletas_esperadas: faker.number.int({ min: 5, max: 20 }),
-//         dias: JSON.stringify(["segunda", "quarta", "sexta"]),
-//         cor: faker.color.rgb()
-//       }
-//     });
-//     zonas.push(zona);
-//   }
+async function main() {
+  const nome = faker.person.fullName();
+  const email = "admin@exemplo.com";
+  const senhaPlana = "123456";
+  const senhaHash = await bcrypt.hash(senhaPlana, 10);
 
-//   // Criar endereços
-//   const enderecos = [];
-//   for (let i = 0; i < 20; i++) {
-//     const endereco = await prisma.endereco.create({
-//       data: {
-//         nome_rua: faker.location.street(),
-//         numero: faker.string.numeric({ length: 3 }),
-//         bairro: faker.location.city(),
-//         id_zona: zonas[i % zonas.length].id
-//       }
-//     });
-//     enderecos.push(endereco);
-//   }
+  const adminExistente = await prisma.admin.findUnique({
+    where: { email }
+  });
 
-//   // Criar usuários
-//   const usuarios = [];
-//   for (let i = 0; i < 5; i++) {
-//     const usuario = await prisma.usuario.create({
-//       data: {
-//         nome: faker.person.fullName(),
-//         email: faker.internet.email(),
-//         senha: faker.internet.password(),
-//         tipo_usuario: "coletor"
-//       }
-//     });
-//     usuarios.push(usuario);
-//   }
+  if (!adminExistente) {
+    await prisma.admin.create({
+      data: {
+        nome,
+        email,
+        senha: senhaHash,
+      }
+    });
+    console.log("Usuário admin criado com sucesso!");
+  } else {
+    console.log("Usuário admin já existe.");
+  }
+}
 
-//   // Criar clientes
-//   const clientes = [];
-//   for (let i = 0; i < 20; i++) {
-//     const cliente = await prisma.cliente.create({
-//       data: {
-//         nome_cliente: faker.person.fullName(),
-//         telefone_cliente: faker.phone.number(),
-//         qr_code: faker.string.uuid(),
-//         id_endereco: enderecos[i % enderecos.length].id_endereco
-//       }
-//     });
-//     clientes.push(cliente);
-//   }
-
-//   // Criar agendamentos
-//   for (let i = 0; i < 20; i++) {
-//     await prisma.agendamento.create({
-//       data: {
-//         dia_agendado: faker.date.future(),
-//         turno_agendado: ["manhã", "tarde", "noite"][i % 3],
-//         observacoes: faker.lorem.sentence(),
-//         id_cliente: clientes[i % clientes.length].id_cliente,
-//         id_usuario: usuarios[i % usuarios.length].id_usuario,
-//         id_zona: zonas[i % zonas.length].id
-//       }
-//     });
-//   }
-
-//   // Criar coletas
-//   for (let i = 0; i < 20; i++) {
-//     await prisma.coleta.create({
-//       data: {
-//         dia_realizado: faker.date.recent(),
-//         horario_realizado: faker.date.anytime(),
-//         id_cliente: clientes[i % clientes.length].id_cliente,
-//         id_usuario: usuarios[i % usuarios.length].id_usuario
-//       }
-//     });
-//   }
-
-//   console.log("✅ Dados inseridos com sucesso!");
-// }
-
-// main()
-//   .catch(e => {
-//     console.error(e);
-//     process.exit(1);
-//   })
-//   .finally(async () => {
-//     await prisma.$disconnect();
-//   });
+main()
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
