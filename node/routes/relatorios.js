33df06c46e5b12538f3bb5166cf2b4343b1af1c6
Vinicius <vinniciussramallho98@gmail.com>
2025-06-16 15:31:43 -0300

    rename directory call

--- node/routes/relatorios.js
+++ node/routes/relatorios.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { montarFiltros, agruparPorCliente } = require("../utils/relatoriosUtils");
+const { montarFiltros, agruparPorCliente } = require("../utils/utilsRelatorios");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
